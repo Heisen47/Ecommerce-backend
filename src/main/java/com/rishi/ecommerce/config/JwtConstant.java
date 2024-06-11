6d5bde955d27e17c10e5834d804c3169a513bdfd
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2024-06-11 13:45:37 +0530

    after hours of debug , two changes were done to fix the problem of 401 error . 1. created an empty constructor of AuthResponse 2. the constant secret_key size is increased to 50+ characters

--- src/main/java/com/rishi/ecommerce/config/JwtConstant.java
+++ src/main/java/com/rishi/ecommerce/config/JwtConstant.java
@@ -2,7 +2,7 @@ package com.rishi.ecommerce.config;
 
 public class JwtConstant {
 
-    public static final String SECRET_KEY= "ngsnksnkndsgknladgnowgkdukdudududu6rdd6uei5wwkuer7lk86t5e6eud";
+    public static final String SECRET_KEY= "ngsnksnkndsgknladgnowgkdukdudududu6rdd6uei5wwkuer7lk86t5e6eud";  //this key should be atleast more than 50 chars long to work
     public static final String JWT_HEADER = "Authorization";
 
 }
